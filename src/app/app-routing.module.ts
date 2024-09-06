505ebeaebe9a1b4497956ff0553652693eb32080
Priyanka.Vasam <priyanka.vasam@ust.com>
2024-09-06 18:25:32 +0530

    layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/admin/layout/layout.component';
+import { DashboardComponent } from './components/admin/views/dashboard/dashboard.component';
+import { TrainingRequestsComponent } from './components/admin/views/training-requests/training-requests.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin', // Base path for the admin section
+    component: LayoutComponent, // Layout component contains the <router-outlet>
+    children: [
+      { path: 'dashboard', component: DashboardComponent }, // Child route (dashboard)
+      {path:'training-requests',component:TrainingRequestsComponent}
+    ],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/admin/layout/layout.component';
+import { DashboardComponent } from './components/admin/views/dashboard/dashboard.component';
+import { TrainingRequestsComponent } from './components/admin/views/training-requests/training-requests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    DashboardComponent,
+    TrainingRequestsComponent
   ],
   imports: [
     BrowserModule,
